677ce43e141647c97efcc52ff40df9f06786be0c
M. Mahdani Rahmatullah <50037482+mahdanidn@users.noreply.github.com>
2022-09-16 12:50:48 +0700

    challenge promises done

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const penontonTheaterIXX = await promiseTheaterIXX();
+    const penontonTheaterVGC = await promiseTheaterVGC();
+    const groupPenonton = [...penontonTheaterIXX, ...penontonTheaterVGC];
+
+    const totalEmosiPenonton = groupPenonton.filter(
+      (penonton) => penonton.hasil == emosi
+    );
+    return console.log(totalEmosiPenonton.length);
+  } catch (error) {
+    console.log("terjadi kesalahan", error);
+  }
+};
+
+promiseOutput("marah");
+promiseOutput("tidak marah");
 
 module.exports = {
   promiseOutput,
